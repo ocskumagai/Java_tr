1fe43012e178a6254564e620893b1b55c27ec805
Kumagai Toshiki <ocs.kumagai.toshiki@gmail.com>
2023-07-31 16:29:46 +0900

    commit

---
+++ java_cr/src/process/Info_food.java
@@ -0,0 +1,30 @@
+package process;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Info_food {
+	String country;
+	String food;
+	String kindOf;
+	
+	public Info_food(String country , String food , 
+					 String kindOf) 
+	{
+        // インスタンス変数に文字を格納
+		this.country = country;
+		this.food = food;
+		this.kindOf = kindOf;
+        // 表示する
+		System.out.println("こんにちは！ここは" + this.country + "です");
+		System.out.println("この"+ this.food + "はうまい");
+		System.out.println(this.food + "は" + this.kindOf + "です" );
+        // 日時を取得する
+		LocalDateTime date = LocalDateTime.now();
+		//表示を変更		
+		DateTimeFormatter dtFormat = 
+		DateTimeFormatter.ofPattern("今の現在日時はyyyy/MM/dd H:m:sです");
+		String formatDate1 = dtFormat.format(date);//表示形式+Stringに変換
+		System.out.println(formatDate1);
+	}
+}
